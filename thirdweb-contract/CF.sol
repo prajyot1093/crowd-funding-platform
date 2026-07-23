08f5fa483b1a9c48080b07c96872b6d3b166f4b0
PRAJYOT PUNDE <prajyot1093@gmail.com>
2026-07-23 20:20:06 +0530

    fix : all functions in contract created and contract is deployed after this commit

--- thirdweb-contract/CF.sol
+++ thirdweb-contract/CF.sol
@@ -25,12 +25,13 @@ contract CF {
     )public returns (uint256) {
 
         campaign storage newCampaign = campaigns[numberofCampaigns];
-        require(newCampaign.deadline < block.timestamp, "deadline should be a date in the future");
+        require(_deadline > block.timestamp, "deadline should be a date in the future");
         newCampaign.owner = _owner;
         newCampaign.title = _title;
         newCampaign.target = _target;
         newCampaign.description = _description;
         newCampaign.ammount = 0;
+        newCampaign.deadline = _deadline;
         newCampaign.img = _image;
        
         numberofCampaigns++;
@@ -40,9 +41,30 @@ contract CF {
 
     }
 
-    function donatetoCampaign() {}
+    function donatetoCampaign(uint256 _id) public payable{
+        uint256 amount = msg.value;
+        campaign storage newCampaign = campaigns[_id];
+        newCampaign.donators.push(msg.sender);
+        newCampaign.donations.push(amount);
 
-    function getDoantors() {}
+        (bool sent ,) = payable(newCampaign.owner).call{value : amount}("");
+        
+        if(sent) {
+            newCampaign.ammount = newCampaign.ammount + amount;
+        }
+    
+    
+    }
 
-    function getCampaigns() {}
+    function getDonators(uint256 _id) public view returns(address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns(campaign[] memory) {
+        campaign[] memory allCampaigns = new campaign[](numberofCampaigns);
+        for(uint i = 0; i < numberofCampaigns; i++){
+            allCampaigns[i] = campaigns[i];
+        }
+        return allCampaigns;
+    }
 }
